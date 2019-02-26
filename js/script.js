a5be9fb139fa9be2d0437390956862cdd60e75c6
Miky Mikusher <mikusher@hotmail.com>
2019-02-26 08:39:24 +0000

    initial mask

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log("Mikusher");
+//local information
+window.onload = function () {
+
+    // variables
+    var btnCheckData = document.getElementById("btnCheckData");
+
+
+
+    //functions 
+    btnCheckData.addEventListener("click", function (params) {
+        get_ajax_data();
+    });
+
+    function get_ajax_data() {
+
+        cveJson = $.ajax({
+            type: "GET",
+            url: "https://cve.circl.lu/api/cve/CVE-2010-3333",
+            dataType: "json",
+            cache: false,
+            success: function (cveDataResult) {
+                console.log(cveDataResult);
+            },
+            error: function (xhr) {
+                alert(xhr.statusText);
+            }
+        });
+
+    } 
+
+};
