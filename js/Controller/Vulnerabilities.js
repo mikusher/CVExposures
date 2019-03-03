d3def6cb8090263011f5418540a6721aef0d8cd7
Luis Amilcar Tavares <mikusher@hotmail.com>
2019-03-03 13:59:48 +0000

    Finalize vulnerabilities page

--- js/Controller/Vulnerabilities.js
+++ js/Controller/Vulnerabilities.js
@@ -1,17 +1,24 @@
 $(document).ready(function () {
 
-    let inputVulnerabilitiesFilter = document.getElementById("inputVulnerabilitiesFilter");
-    let vulnerabilitiesFilter = document.getElementById("vulnerabilitiesFilter");
-    let allVendorsCT = document.getElementById("allVendors");
+    $('[data-toggle="tooltip"]').tooltip();
+    let positionAlert = document.getElementById("alert-target");
+    let allVendorsInPageVul = document.getElementById("allVendorsInPageVul");
 
-    Utils.loadToContainer("allVendors", 'vd');
+    let btCheckProductContent = document.getElementById("btCheckProductContent");
+
+    let inputSearchInTable = document.getElementById("inputSearchInTable");
+    let btSearchInTable = document.getElementById("btSearchInTable");
+
+    var vulnearbilitiesConatinerTable = document.getElementById("list-table-vulnerabilities");
+
+    Utils.loadToContainer("allVendorsInPageVul", 'vd');
 
 
-    allVendorsCT.addEventListener("change", function () {
+    allVendorsInPageVul.addEventListener("change", function () {
         var newVendorID = $(this).val();
         if (newVendorID !== '') {
             $.getJSON('https://cve.circl.lu/api/browse/'+newVendorID, function (obj) {
-                Utils.loadToContainer("allVendorProduct", 'pd', obj);
+                Utils.loadToContainer("allProductInPageVul", 'pd', obj);
             });
         } else {
             $('#allVendors').html('<option value="">Select Vendor</option>');
@@ -21,14 +28,45 @@ $(document).ready(function () {
 
 
     //action to filter button
-    vulnerabilitiesFilter.addEventListener("click", function () {
-        let value = $(inputVulnerabilitiesFilter).val().toLowerCase();
+    btSearchInTable.addEventListener("click", function () {
+        let value = $(inputSearchInTable).val().toLowerCase();
 
         $("table tr").filter(function (index) {
             if (index > 0) {
                 $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
             }
         });
+    });
+
+
+    btCheckProductContent.addEventListener("click", function (params) {
+
+        let selectComboVendors = document.getElementById("allVendorsInPageVul").value;
+        let selectComboProduct = document.getElementById("allProductInPageVul").value;
+
+        if (selectComboVendors === 'Select Vendor' || selectComboProduct === 'Select Product'){
+            selectComboVendors = null;
+            selectComboProduct = null;
+        }
+
+        if( selectComboVendors !== null && selectComboProduct !== null ){
+            let urlResult = 'https://cve.circl.lu/api/search/'+selectComboVendors+'/'+selectComboProduct;
+            $.getJSON(urlResult, function (obj) {
+                for (const individualIndex of obj) {
+                    //console.log(individualIndex);
+                    let tr = document.createElement('tr');
+                    tr.innerHTML = `
+                                <td id='${individualIndex.id}'>${individualIndex.id}</td>
+                                <td title='${individualIndex.summary}' data-toggle="tooltip" data-placement="top" style="text-overflow: ellipsis; max-width: 810px;">${individualIndex.summary}</td>
+                                
+                                `;
+
+                    vulnearbilitiesConatinerTable.appendChild(tr);
+                }
+            });
+        }else {
+            positionAlert.style.display = (positionAlert.style.display === "none") ? "block" : "none";
+        }
     });
 
 
